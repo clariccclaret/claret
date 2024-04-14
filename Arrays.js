2748b3d53d997eb2c7fcb351ec195f0a7f8ef26d
clariccclaret <164161865+clariccclaret@users.noreply.github.com>
2024-04-14 12:36:57 +0300

    Add files via upload

---
+++ Arrays.js
@@ -0,0 +1,11 @@
+let students= ["Harry","meycy","cardym","cliter"];
+
+console.log(students);
+ students.unshift("will");
+ console.log(students);
+ students.push("el");
+ console.log(students);
+ students.pop("cliter");
+ console.log(students);
+ students.shift("will");
+ console.log(students);
